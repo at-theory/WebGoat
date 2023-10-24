4aac7929658662f4f216ebe8387198e3724d89da
at-theory <145712853+at-theory@users.noreply.github.com>
2023-10-24 14:53:04 -0700

    Trigger semgrep detection

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionQuiz.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionQuiz.java
@@ -18,6 +18,7 @@
  * Getting Source ==============
  *
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
+ * at semgrep test
  */
 
 package org.owasp.webgoat.lessons.sqlinjection.advanced;
